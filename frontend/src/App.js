65850c6452cd862a81d47c0524ae9f49ed9b8307
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-07-08 17:17:22 -0300

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
+import GlobalStyle from "./styles/global";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <>
+   <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
+    <GlobalStyle />
+   </>
   );
 }
 
