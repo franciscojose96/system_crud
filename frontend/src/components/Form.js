6e51b7ec709842c413715d0640a29dbb0bbea774
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-07-09 10:00:46 -0300

    Create Form.js

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,20 @@
+import Form from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.form 
+    `display: flex;
+    align-items: flex-end;
+    gap: 10px;
+    flex-wrap: wrap;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 5px;`
+
+const Form = () => {
+    return (
+        <></>
+    );
+};
+
+export default Form;
