f7b0ea3617c7b10ec1c015d28e8be59471bed9ea
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-07-09 17:22:54 -0300

    exibition-users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import GlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./components/form.js"; // ← corrige o import do Form
+import Grid from "./components/Grid.js"
+import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import React from "react";
+import axios from "axios";
+import GlobalStyle  from './styles/global';
+
+
 
 const Container = styled.div`
   width: 100%;
@@ -18,11 +22,29 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
         <Form />
+        <Grid users={users} />
       </Container>
       <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
