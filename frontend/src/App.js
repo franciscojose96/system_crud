02576425d436f7ddc4c4ad56d13839db8a4895b0
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-07-09 15:01:59 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
-import Form from "styled-components";
+import Form from "./components/form.js"; // ← corrige o import do Form
 import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 import React from "react";
 
 const Container = styled.div`
   width: 100%;
-  mex-width: 800px;
+  max-width: 800px; /* corrigido aqui */
   margin-top: 20px;
   display: flex;
   flex-direction: column;
@@ -15,18 +15,18 @@ const Container = styled.div`
   gap: 10px;
 `;
 
-
 const Title = styled.h2``;
+
 function App() {
   return (
-   <>
-  <Container>
-    <Title>USÚARIOS</Title>
-    <Form/>
-  </Container>
-   <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
-    <GlobalStyle />
-   </>
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+      </Container>
+      <ToastContainer autoClose={3000} position="bottom-left" />
+      <GlobalStyle />
+    </>
   );
 }
 
